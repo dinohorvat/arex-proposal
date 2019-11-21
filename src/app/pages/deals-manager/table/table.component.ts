d35d8c08099d602fd980d3d72be5a6a7feb9b412
Dino Horvat <dxh3401@rit.edu>
2019-11-21 23:42:48 +0100

    added favorites star

--- src/app/pages/deals-manager/table/table.component.ts
+++ src/app/pages/deals-manager/table/table.component.ts
@@ -9,12 +9,12 @@ import { DealModel } from '../../../models/deal.model';
 export class DealsManagerTableComponent implements OnInit {
   @Input() deals: DealModel[];
   columnDefinition = [
-    { field: 'name', header: 'Name', type: true },
-    { field: 'address', header: 'Address' },
-    { field: 'price', header: 'Price', sortable: true },
-    { field: 'type', header: 'Type', sortable: true, className: 'type' },
-    { field: 'dueDate', header: 'Due Date', sortable: true },
-    { header: '' }
+    { field: 'name', header: 'Name', width: '30%' },
+    { field: 'address', header: 'Address', type: 'address', width: '30%' },
+    { field: 'price', header: 'Price', sortable: true, width: '20%' },
+    { field: 'type', header: 'Type', type: 'type', sortable: true, className: 'type', width: '20%' },
+    { field: 'dueDate', header: 'Due Date', sortable: true, width: '10%' },
+    { header: '', type: 'favorites', width: '5%', className: 'favorites' }
   ];
   constructor() { }
 
