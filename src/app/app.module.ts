701a2f7b74894e06c32ce6472f48818853e222de
Dino Horvat <dxh3401@rit.edu>
2019-11-22 01:23:13 +0100

    fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,11 +13,6 @@ import { AppComponent } from './app.component';
 // Import containers
 import { DefaultLayoutComponent } from './containers';
 
-import { P404Component } from './views/error/404.component';
-import { P500Component } from './views/error/500.component';
-import { LoginComponent } from './views/login/login.component';
-import { RegisterComponent } from './views/register/register.component';
-
 const APP_CONTAINERS = [
   DefaultLayoutComponent
 ];
@@ -62,10 +57,6 @@ import { HttpClientModule } from '@angular/common/http';
   declarations: [
     AppComponent,
     ...APP_CONTAINERS,
-    P404Component,
-    P500Component,
-    LoginComponent,
-    RegisterComponent
   ],
   providers: [
       {
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -4,15 +4,10 @@ import { Routes, RouterModule } from '@angular/router';
 // Import Containers
 import { DefaultLayoutComponent } from './containers';
 
-import { P404Component } from './views/error/404.component';
-import { P500Component } from './views/error/500.component';
-import { LoginComponent } from './views/login/login.component';
-import { RegisterComponent } from './views/register/register.component';
-
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: 'dashboard',
+    redirectTo: 'deals-manager',
     pathMatch: 'full',
   },
   {
@@ -26,41 +21,8 @@ export const routes: Routes = [
         path: 'deals-manager',
         loadChildren: () => import('./pages/deals-manager/deals-manager.module').then(m => m.DealsManagerModule)
       },
-      {
-        path: 'base',
-        loadChildren: () => import('./views/base/base.module').then(m => m.BaseModule)
-      },
-      {
-        path: 'buttons',
-        loadChildren: () => import('./views/buttons/buttons.module').then(m => m.ButtonsModule)
-      },
-      {
-        path: 'charts',
-        loadChildren: () => import('./views/chartjs/chartjs.module').then(m => m.ChartJSModule)
-      },
-      {
-        path: 'dashboard',
-        loadChildren: () => import('./views/dashboard/dashboard.module').then(m => m.DashboardModule)
-      },
-      {
-        path: 'icons',
-        loadChildren: () => import('./views/icons/icons.module').then(m => m.IconsModule)
-      },
-      {
-        path: 'notifications',
-        loadChildren: () => import('./views/notifications/notifications.module').then(m => m.NotificationsModule)
-      },
-      {
-        path: 'theme',
-        loadChildren: () => import('./views/theme/theme.module').then(m => m.ThemeModule)
-      },
-      {
-        path: 'widgets',
-        loadChildren: () => import('./views/widgets/widgets.module').then(m => m.WidgetsModule)
-      }
     ]
-  },
-  { path: '**', component: P404Component }
+  }
 ];
 
 @NgModule({
